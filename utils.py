5a9977e7cbed1afd6ecab27102a4dae9f163e776
chachacode <103096938+ChaChaCode@users.noreply.github.com>
2025-01-23 16:00:24 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,23 +1,25 @@
 import logging
 from aiogram import Bot, types
-from aiogram.types import FSInputFile
+from aiogram.types import FSInputFile, Message
 
-
-async def send_message_with_image(bot: Bot, chat_id: int, text: str, reply_markup=None, message_id: int = None):
-    image_path = 'image/pepes.png'
+async def send_message_with_image(bot: Bot, chat_id: int, text: str, reply_markup=None, message_id: int = None) -> Message | None:
+    image_path = 'image/pepes.png'  # Замените на путь к вашему изображению
     image = FSInputFile(image_path)
 
-    if message_id:
-        try:
-            await bot.edit_message_media(
+    try:
+        if message_id:
+            # Редактируем сообщение
+            return await bot.edit_message_media(
                 chat_id=chat_id,
                 message_id=message_id,
                 media=types.InputMediaPhoto(media=image, caption=text),
                 reply_markup=reply_markup
             )
-        except Exception as e:
-            logging.error(f"Error editing message with image: {e}")
-            await bot.send_photo(chat_id=chat_id, photo=image, caption=text, reply_markup=reply_markup)
-    else:
-        await bot.send_photo(chat_id=chat_id, photo=image, caption=text, reply_markup=reply_markup)
+        else:
+            # Отправляем новое сообщение, если message_id не указан
+            return await bot.send_photo(chat_id=chat_id, photo=image, caption=text, reply_markup=reply_markup)
+    except Exception as e:
+        logging.error(f"Error in send_message_with_image: {str(e)}")
+        # Если не получилось отредактировать или отправить, логируем ошибку и возвращаем None
+        return None
 
