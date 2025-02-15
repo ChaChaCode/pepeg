175138cadaa555a97e86a3384a11a260eb2194a5
chachacode <103096938+ChaChaCode@users.noreply.github.com>
2025-02-15 18:02:34 +0500

    Update new_public.py

--- new_public.py
+++ new_public.py
@@ -1,13 +1,16 @@
-from typing import Dict
-import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
+from aiogram.filters import ChatMemberUpdatedFilter, IS_MEMBER
+from aiogram.types import ChatMemberUpdated, InlineKeyboardMarkup, InlineKeyboardButton, ChatMemberAdministrator
 from supabase import create_client, Client
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 from utils import send_message_with_image
 from aiogram.enums import ChatMemberStatus
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 # Bot configuration and initialization
 BOT_TOKEN = '7908502974:AAHypTBbfW-c9JR94HNYFLL9ZcN-2LaJFoU'
@@ -20,126 +23,190 @@ supabase_url = 'https://olbnxtiigxqcpailyecq.supabase.co'
 supabase_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sYm54dGlpZ3hxY3BhaWx5ZWNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAxMjQwNzksImV4cCI6MjA0NTcwMDA3OX0.dki8TuMUhhFCoUVpHrcJo4V1ngKEnNotpLtZfRjsePY'
 supabase: Client = create_client(supabase_url, supabase_key)
 
-user_selected_communities = {}
-paid_users: Dict[int, str] = {}
 
-
-# States for the FSM
 class GiveawayStates(StatesGroup):
-    waiting_for_name = State()
-    waiting_for_description = State()
-    waiting_for_media_choice = State()
-    waiting_for_media_upload = State()
-    waiting_for_end_time = State()
-    waiting_for_winner_count = State()
-    waiting_for_community_name = State()
-    waiting_for_new_end_time = State()
-    waiting_for_media_edit = State()
-    waiting_for_congrats_message = State()
-    waiting_for_common_congrats_message = State()
-    waiting_for_edit_name = State()
-    waiting_for_edit_description = State()
-    waiting_for_edit_winner_count = State()
-    creating_giveaway = State()
     binding_communities = State()
+
+
+# Dictionary to store pending channel bindings
+pending_channels = {}
+
 
 def register_new_public(dp: Dispatcher, bot: Bot, supabase: Client):
     @dp.callback_query(lambda c: c.data.startswith('bind_new_community:'))
     async def process_bind_new_community(callback_query: types.CallbackQuery, state: FSMContext):
         giveaway_id = callback_query.data.split(':')[1]
-        await state.update_data(giveaway_id=giveaway_id)
-        await state.set_state(GiveawayStates.waiting_for_community_name)
+        user_id = callback_query.from_user.id
+        message_id = callback_query.message.message_id
+
+        await state.set_state(GiveawayStates.binding_communities)
+        await state.update_data(giveaway_id=giveaway_id, message_id=message_id)
+        pending_channels[user_id] = {
+            'giveaway_id': giveaway_id,
+            'message_id': message_id
+        }
+
+        logging.info(f"User {user_id} started binding process for giveaway {giveaway_id}")
+
         await bot.answer_callback_query(callback_query.id)
 
-        keyboard = InlineKeyboardBuilder()
-        keyboard.button(text="Назад", callback_data=f"bind_communities:{giveaway_id}")
-        keyboard.adjust(1)
+        keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="Назад", callback_data=f"bind_communities:{giveaway_id}")]
+        ])
 
         await send_message_with_image(
             bot,
-            callback_query.from_user.id,
-            "Чтобы привязать паблик, вы должны добавить этого бота @PepeGift_Bot в администраторы вашего паблика. После этого скиньте имя паблика пример: @publik",
-            reply_markup=keyboard.as_markup(),
-            message_id=callback_query.message.message_id
+            user_id,
+            "Чтобы привязать паблик, добавьте этого бота @PepeGift_Bot в администраторы вашего паблика или группы. "
+            "Бот автоматически обнаружит новый паблик/группу и привяжет его к розыгрышу.\n\n"
+            "Пожалуйста, убедитесь, что при добавлении бота как администратора вы предоставили следующие права:\n"
+            "- Публикация сообщений\n"
+            "- Редактирование сообщений\n"
+            "- Добавление подписчиков\n\n"
+            "Также, не находясь в состоянии привязки паблика, вы можете добавить бота в качестве администратора сразу в нескольких пабликах. После этого, перейдя в состояние привязки, вы увидите все паблики, в которых бот уже назначен администратором.\n\n"
+            "Эти права должны быть включены автоматически при добавлении бота. Не изменяйте стандартный набор прав при добавлении.\n",
+            reply_markup=keyboard,
+            message_id=message_id
         )
-        await state.update_data(last_message_id=callback_query.message.message_id)
 
-    @dp.message(GiveawayStates.waiting_for_community_name)
-    async def process_community_name(message: types.Message, state: FSMContext):
-        data = await state.get_data()
-        giveaway_id = data['giveaway_id']
-        last_message_id = data.get('last_message_id')
-        last_error_message = data.get('last_error_message', '')
+    @dp.my_chat_member(ChatMemberUpdatedFilter(member_status_changed=IS_MEMBER))
+    async def bot_added_as_admin(event: ChatMemberUpdated, state: FSMContext):
+        chat = event.chat
+        user_id = event.from_user.id
 
-        if not message.text.startswith('@'):
-            await handle_invalid_input(message, state, giveaway_id, last_message_id, last_error_message)
-            await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
+        logging.info(
+            f"Статус бота изменен в канале/группе {chat.id} пользователем {user_id}. Новый статус: {event.new_chat_member.status}")
+
+        state_data = await state.get_data()
+        pending_data = pending_channels.get(user_id, {})
+
+        giveaway_id = state_data.get('giveaway_id') or pending_data.get('giveaway_id')
+        message_id = state_data.get('message_id') or pending_data.get('message_id')
+
+        if not giveaway_id:
+            # Бот добавлен не во время процесса привязки к розыгрышу
+            if event.new_chat_member.status == ChatMemberStatus.ADMINISTRATOR:
+                # Записываем информацию в базу данных
+                await record_bound_community(user_id, chat.username, str(chat.id))
+                logging.info(
+                    f"Бот добавлен как администратор в {chat.username} (ID: {chat.id}) пользователем {user_id}")
             return
 
-        channel_username = message.text[1:]  # Remove "@" prefix
+        # Всегда сохраняем giveaway_id и message_id в состоянии
+        await state.update_data(giveaway_id=giveaway_id, message_id=message_id)
 
-        try:
-            chat = await bot.get_chat(f"@{channel_username}")
+        if event.new_chat_member.status == ChatMemberStatus.ADMINISTRATOR:
+            # Проверяем права бота
             bot_member = await bot.get_chat_member(chat.id, bot.id)
+            if isinstance(bot_member, ChatMemberAdministrator):
+                required_permissions = {
+                    'can_post_messages': 'Публикация сообщений',
+                    'can_edit_messages': 'Редактирование сообщений',
+                    'can_invite_users': 'Добавление подписчиков'
+                }
 
-            if bot_member.status == ChatMemberStatus.ADMINISTRATOR:
-                await handle_successful_binding(message, state, giveaway_id, channel_username, last_message_id)
+                missing_permissions = [
+                    perm_name for perm, perm_name in required_permissions.items()
+                    if not getattr(bot_member, perm, False)
+                ]
+
+                if missing_permissions:
+                    missing_perms_str = ', '.join(missing_permissions)
+                    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                        [InlineKeyboardButton(text="Назад", callback_data=f"bind_communities:{giveaway_id}")]
+                    ])
+                    await send_message_with_image(
+                        bot,
+                        user_id,
+                        f"Вы ограничили права боту как администратору. Пожалуйста, удалите бота из администраторов и повторите добавление бота, предоставив следующие права:\n\n{missing_perms_str}\n\nЭти права должны быть включены автоматически при добавлении бота как администратора. Пожалуйста, не изменяйте стандартный набор прав при добавлении.",
+                        reply_markup=keyboard,
+                        message_id=message_id
+                    )
+                else:
+                    await handle_successful_binding(chat.id, chat.username, user_id, giveaway_id, state, message_id)
+                    if user_id in pending_channels:
+                        del pending_channels[user_id]
             else:
-                await handle_not_admin(message, state, giveaway_id, last_message_id, last_error_message)
-        except ValueError:
-            await handle_channel_not_found(message, state, giveaway_id, last_message_id, last_error_message)
-        except Exception as e:
-            await handle_general_error(message, state, giveaway_id, last_message_id, last_error_message)
+                keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                    [InlineKeyboardButton(text="Назад", callback_data=f"bind_communities:{giveaway_id}")]
+                ])
+                await send_message_with_image(
+                    bot,
+                    user_id,
+                    "Бот не был добавлен как администратор или произошла ошибка при проверке прав. Пожалуйста, убедитесь, что вы добавили бота как администратора с необходимыми правами.",
+                    reply_markup=keyboard,
+                    message_id=message_id
+                )
+        elif event.new_chat_member.status == ChatMemberStatus.MEMBER:
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                [InlineKeyboardButton(text="Назад", callback_data=f"bind_communities:{giveaway_id}")]
+            ])
+            await send_message_with_image(
+                bot,
+                user_id,
+                "Бот был добавлен как обычный участник, а не как администратор. Пожалуйста, добавьте бота как администратора с необходимыми правами.",
+                reply_markup=keyboard,
+                message_id=message_id
+            )
 
-        await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-
-    async def handle_invalid_input(message: types.Message, state: FSMContext, giveaway_id: str, last_message_id: int,
-                                   last_error_message: str):
-        new_error_message = "Пожалуйста, введите имя, начиная с @. Попробуйте еще раз."
-        await update_error_message(message, state, giveaway_id, last_message_id, last_error_message, new_error_message)
-
-    async def handle_successful_binding(message: types.Message, state: FSMContext, giveaway_id: str,
-                                        channel_username: str,
-                                        last_message_id: int):
+    async def handle_successful_binding(channel_id: int, channel_username: str, user_id: int, giveaway_id: str,
+                                        state: FSMContext, message_id: int):
         try:
-            # Получаем информацию о канале
-            chat = await bot.get_chat(f"@{channel_username}")
-            channel_id = chat.id
-
-            # Проверяем, не привязан ли уже этот паблик к розыгрышу
             response = supabase.table('giveaway_communities').select('*').eq('giveaway_id', giveaway_id).eq(
-                'community_id',
-                str(channel_id)).execute()
+                'community_id', str(channel_id)).execute()
+
             if response.data:
-                new_error_message = f"Паблик \"{channel_username}\" уже привязан к этому розыгрышу."
-                await update_error_message(message, state, giveaway_id, last_message_id, "", new_error_message)
+                await send_message_with_image(
+                    bot,
+                    user_id,
+                    f"Паблик \"{channel_username}\" уже привязан к этому розыгрышу.",
+                    message_id=message_id
+                )
                 return
 
-            # Привязываем сообщество к розыгрышу, используя ID канала
             await bind_community_to_giveaway(giveaway_id, str(channel_id), channel_username)
+            await record_bound_community(user_id, channel_username, str(channel_id))
 
-            # Record the bound community
-            await record_bound_community(message.from_user.id, channel_username, str(channel_id))
-
-            keyboard = InlineKeyboardBuilder()
-            keyboard.button(text="Назад", callback_data=f"bind_communities:{giveaway_id}")
-            keyboard.adjust(1)
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                [InlineKeyboardButton(text="Назад", callback_data=f"bind_communities:{giveaway_id}")]
+            ])
 
             await send_message_with_image(
                 bot,
-                message.chat.id,
+                user_id,
                 f"Паблик \"{channel_username}\" успешно привязан к розыгрышу!",
-                reply_markup=keyboard.as_markup(),
-                message_id=last_message_id
+                reply_markup=keyboard,
+                message_id=message_id
             )
             await state.clear()
+            logging.info(f"Successfully bound channel {channel_id} to giveaway {giveaway_id} for user {user_id}")
         except Exception as e:
             logging.error(f"Error in handle_successful_binding: {str(e)}")
-            await handle_general_error(message, state, giveaway_id, last_message_id, "")
+            await send_message_with_image(
+                bot,
+                user_id,
+                "Произошла ошибка при привязке паблика. Пожалуйста, попробуйте еще раз.",
+                message_id=message_id
+            )
+
+    async def fetch_latest_giveaway(user_id: int):
+        try:
+            response = supabase.table('giveaways').select('id').eq('creator_id', str(user_id)).order('created_at',
+                                                                                                     desc=True).limit(
+                1).execute()
+            if response.data:
+                giveaway_id = response.data[0]['id']
+                logging.info(f"Retrieved latest giveaway ID {giveaway_id} for user {user_id}")
+                return giveaway_id, None  # We don't have a message_id in this case
+            else:
+                logging.warning(f"No giveaways found for user {user_id}")
+                return None, None
+        except Exception as e:
+            logging.error(f"Error fetching latest giveaway: {str(e)}")
+            return None, None
 
     async def record_bound_community(user_id: int, community_username: str, community_id: str):
         try:
-            # Check if community is already recorded for the user
             response = supabase.table('bound_communities').select('*').eq('user_id', user_id).eq('community_id',
                                                                                                  community_id).execute()
             if response.data:
@@ -161,42 +228,13 @@ def register_new_public(dp: Dispatcher, bot: Bot, supabase: Client):
             logging.error(f"Error recording bound community: {str(e)}")
             return False
 
-    async def handle_not_admin(message: types.Message, state: FSMContext, giveaway_id: str, last_message_id: int,
-                               last_error_message: str):
-        new_error_message = f"Бот не является администратором в паблике \"{message.text}\". Пожалуйста, добавьте бота в администраторы и попробуйте снова."
-        await update_error_message(message, state, giveaway_id, last_message_id, last_error_message, new_error_message)
-
-    async def handle_channel_not_found(message: types.Message, state: FSMContext, giveaway_id: str,
-                                       last_message_id: int,
-                                       last_error_message: str):
-        new_error_message = "Не удалось найти паблик с таким именем. Пожалуйста, проверьте правильность ссылки и попробуйте снова."
-        await update_error_message(message, state, giveaway_id, last_message_id, last_error_message, new_error_message)
-
-    async def handle_general_error(message: types.Message, state: FSMContext, giveaway_id: str, last_message_id: int,
-                                   last_error_message: str):
-        new_error_message = "Скорее всего, указано неверное название паблика. Пожалуйста, проверьте правильность ввода и попробуйте снова."
-        await update_error_message(message, state, giveaway_id, last_message_id, last_error_message, new_error_message)
-
-    async def update_error_message(message: types.Message, state: FSMContext, giveaway_id: str, last_message_id: int,
-                                   last_error_message: str, new_error_message: str):
-        if new_error_message != last_error_message:
-            keyboard = InlineKeyboardBuilder()
-            keyboard.button(text="Назад", callback_data=f"bind_communities:{giveaway_id}")
-            keyboard.adjust(1)
-            await send_message_with_image(
-                bot,
-                message.chat.id,
-                new_error_message,
-                reply_markup=keyboard.as_markup(),
-                message_id=last_message_id
-            )
-            await state.update_data(last_error_message=new_error_message)
-        await state.set_state(GiveawayStates.waiting_for_community_name)
-
     async def bind_community_to_giveaway(giveaway_id, community_id, community_username):
         data = {
             "giveaway_id": giveaway_id,
             "community_id": community_id,
             "community_username": community_username
         }
-        supabase.table("giveaway_communities").insert(data).execute()
+        response = supabase.table("giveaway_communities").insert(data).execute()
+        logging.info(f"Bound community {community_id} to giveaway {giveaway_id}: {response.data}")
+
+    return dp
