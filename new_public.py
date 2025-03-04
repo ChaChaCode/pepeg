d5a62beb41e9ace28dc5d52f92a19667360effe8
chachacode <103096938+ChaChaCode@users.noreply.github.com>
2025-03-04 20:22:55 +0500

    Update new_public.py

--- new_public.py
+++ new_public.py
@@ -19,7 +19,7 @@ from datetime import datetime
 logging.basicConfig(level=logging.INFO)
 
 # Конфигурация и инициализация бота
-BOT_TOKEN = '7908502974:AAHypTBbfW-c9JR94HNYFLL9ZcN-2LaJFoU'
+BOT_TOKEN = '7924714999:AAFUbKWC--s-ff2DKe6g5Sk1C2Z7yl7hh0c'
 bot = Bot(token=BOT_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
