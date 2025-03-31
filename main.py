a865e4fc3d2fed3bd0425b0ed37daead1e5f5403
chachacode <103096938+ChaChaCode@users.noreply.github.com>
2025-03-31 16:52:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ class SpamProtectionMiddleware(BaseMiddleware):
             logging.info(f"Пользователь {user_id}: {actions_count + 1} действий за последнюю секунду")
 
             # Проверяем лимит
-            if len(user_actions[user_id]) > 2:
+            if len(user_actions[user_id]) > 3:
                 blocked_users[user_id] = current_time + 60
                 logging.info(f"Пользователь {user_id} заблокирован за спам до {blocked_users[user_id]}")
                 if isinstance(event, types.Message):
