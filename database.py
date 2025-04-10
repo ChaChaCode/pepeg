934222b966242cb9266b77cbdc5413eb47741301
chachacode <103096938+ChaChaCode@users.noreply.github.com>
2025-04-10 21:54:31 +0500

    Add files via upload

---
+++ database.py
@@ -0,0 +1,22 @@
+import psycopg2
+import logging
+
+# Конфигурация PostgreSQL
+db_config = {
+    "host": "91.84.97.19",
+    "port": 5432,
+    "database": "snapidb",
+    "user": "postgres",
+    "password": "moxy1337"
+}
+
+# Подключение к PostgreSQL
+try:
+    conn = psycopg2.connect(**db_config)
+    logging.info("Успешно подключились к PostgreSQL!")
+except Exception as e:
+    logging.error(f"Ошибка подключения к PostgreSQL: {e}")
+    raise
+
+# Создай курсор для выполнения запросов
+cursor = conn.cursor()
